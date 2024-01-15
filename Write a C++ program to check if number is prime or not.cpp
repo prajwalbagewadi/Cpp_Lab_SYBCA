0f7be56961ee3a731542c834433da7247309f37d
Prajwal Bagewadi <77821176+prajwalbagewadi@users.noreply.github.com>
2024-01-15 20:20:49 +0530

    Update Write a C++ program to check if number is prime or not.cpp

--- Write a C++ program to check if number is prime or not.cpp
+++ Write a C++ program to check if number is prime or not.cpp
@@ -2,9 +2,16 @@
 using namespace std;
 bool check(int num)
 {
-    if(num%2==1)
+    if(num%1==0 && num%num==0)
     {
-        return 1;
+        if(num%2==0)
+        {
+            return 0;
+        }    
+        else
+        {
+            return 1;
+        }
     }
     else
     {
@@ -27,3 +34,28 @@ int main()
     }
     return 0;
 }
+
+/*
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    cout<<"Hello world!";
+    int i=67;
+    int ans1=i%1;
+    cout<<i<<"%1="<<ans1<<endl;
+    int ans2=i%i;
+    cout<<i<<"%"<<i<<"="<<ans2<<endl;
+    int ans3=i%2;
+    cout<<i<<"%2="<<ans3<<endl;
+    return 0;
+}
+
+
+Output:
+
+Hello world!67%1=0
+67%67=0
+67%2=1
+*/
