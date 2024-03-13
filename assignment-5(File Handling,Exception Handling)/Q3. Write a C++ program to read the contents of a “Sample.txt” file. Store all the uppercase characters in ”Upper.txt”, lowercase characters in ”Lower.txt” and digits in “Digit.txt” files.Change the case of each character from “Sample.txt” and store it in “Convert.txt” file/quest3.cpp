62ae9d876e8b5c46deab678fa812e7ff6320ed7c
Prajwal Bagewadi <77821176+prajwalbagewadi@users.noreply.github.com>
2024-03-13 20:23:37 +0530

    Add files via upload

---
+++ assignment-5(File Handling,Exception Handling)/Q3. Write a C++ program to read the contents of a “Sample.txt” file. Store all the uppercase characters in ”Upper.txt”, lowercase characters in ”Lower.txt” and digits in “Digit.txt” files.Change the case of each character from “Sample.txt” and store it in “Convert.txt” file/quest3.cpp
@@ -0,0 +1,38 @@
+/*
+Q3. Write a C++ program to read the contents of a “Sample.txt” file. Store all the uppercase
+characters in ”Upper.txt”, lowercase characters in ”Lower.txt” and digits in “Digit.txt”
+files.Change the case of each character from “Sample.txt” and store it in “Convert.txt” file.
+*/
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+int main(){
+    fstream read_obj,write_obj,write_obj1,write_obj2,write_obj3;
+    read_obj.open("sample.txt",ios::in);
+    write_obj.open("upper.txt",ios::out);
+    write_obj1.open("lower.txt",ios::out);
+    write_obj2.open("digit.txt",ios::out);
+    write_obj3.open("convert.txt",ios::out);
+
+    char buf;
+    while (!read_obj.eof()){
+        read_obj.get(buf);
+        //upper
+        if(isupper(buf)){
+            write_obj<<buf;
+            write_obj3.put(tolower(buf));
+        }
+        //lower
+        if(islower(buf)){
+            write_obj1<<buf;
+            write_obj3.put(toupper(buf));
+        }
+        //digit
+        if(isdigit(buf)){
+            write_obj2<<buf;
+        }
+    }
+    
+    return 0;
+}
